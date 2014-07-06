4ab7dd8ba3206f7ea228e8efd3bd76f214546341
yongcing <yong.cing723@gmail.com>
2014-07-06 15:05:13 +0800

    change login error message (invalid ip)

--- application/views/invalid_ip.php
+++ application/views/invalid_ip.php
@@ -1,1 +1,2 @@
 <?php echo "Invalid IP: $ip"; ?>
+<br><button class="btn btn-default" type="button" onclick="javascript:location.href='<?php echo $this->config->base_url('netManager/admin_login');?>'">Admin Login</button>

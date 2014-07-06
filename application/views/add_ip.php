ad90c59aef7e6e6ed890f02529b306bb842de9f0
yongcing <yong.cing723@gmail.com>
2014-07-06 19:11:04 +0800

    IP setting(add/delete) finish

--- application/views/add_ip.php
+++ application/views/add_ip.php
@@ -17,14 +17,26 @@
 <?php
 echo form_open('netManager/set_ip')
 ?>
+
+<br>
+<legend><strong>Add IP</strong></legend>
+
 <table>
 	<fieldset>
-		<label for="account"><b>Account</b></label>
-		<input type="text" name="account" style="width:30.769em" placeholder="" value="<?php echo set_value('account'); ?>"/>
+		<font size="4"><b>Account&nbsp</b></font>
+        <select name="account">
+        <?php foreach($account as $row): ?>
+            <option value="<?php echo $row->account; ?>"><?php echo $row->account; ?></option>
+        <?php endforeach; ?>
+        </select>
 		<br />
 
-		<label for="ip"><b>IP</b></label>
-		<input type="text" name="ip" style="width:30.769em" placeholder="" value="<?php echo set_value('ip'); ?>"/>
+        <font size="4"><b>IP&nbsp</b></font>
+        <select name="ip">
+        <?php foreach($add_ip as $row): ?>
+            <option value="<?php echo $row->ip; ?>"><?php echo $row->ip; ?></option>
+        <?php endforeach; ?>
+        </select>
 		<br />
 
 		<button type="button" name="button" class="btn" onclick="this.form.submit();">Add</button>
@@ -34,18 +46,27 @@ echo form_open('netManager/set_ip')
 </form>
 
 <br>
-<legend> </legend>
+<legend><strong>Remove IP</strong></legend>
 
 <?php
 echo form_open('netManager/delete_ip')
 ?>
 	<fieldset>
-		<label for="account"><b>Account</b></label>
-		<input type="text" name="account" style="width:30.769em" placeholder="" value="<?php echo set_value('account'); ?>"/>
+		<font size="4"><b>Account&nbsp</b></font>
+        <select name="account">
+        <?php foreach($account as $row): ?>
+            <option value="<?php echo $row->account ?>"><?php echo $row->account; ?></option>
+        <?php endforeach; ?>
+        </select>
+
 		<br />
 
-		<label for="ip"><b>IP</b></label>
-		<input type="text" name="ip" style="width:30.769em" placeholder="" value="<?php echo set_value('ip'); ?>"/>
+        <font size="4"><b>IP&nbsp</b></font>
+        <select name="ip">
+        <?php foreach($del_ip as $row): ?>
+            <option value="<?php echo $row->ip ?>"><?php echo $row->ip; ?></option>
+        <?php endforeach; ?>
+        </select>
 		<br />
     
 		<button type="button" name="button" class="btn" onclick="this.form.submit();">Delete</button>
